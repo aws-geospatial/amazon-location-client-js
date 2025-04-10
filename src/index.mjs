eef907077948386358b80f17bb4e659c81bee06a
Chris Galvan <chgalvan@amazon.com>
2025-04-10 11:35:02 -0500

    Added unit tests (#23)

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 export * from "@aws-sdk/client-location";
 export * from "@aws-sdk/credential-providers";
 
---
+++ tests/index.test.js
@@ -0,0 +1,30 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+  withAPIKey,
+  withIdentityPoolId,
+  GeoMapsClient,
+  GeoPlacesClient,
+  GeoRoutesClient,
+  maps,
+  places,
+  routes,
+} from "../src/index";
+
+test("can import authentication helper methods", () => {
+  expect(withAPIKey).toBeDefined();
+  expect(withIdentityPoolId).toBeDefined();
+});
+
+test("can import standalone clients", () => {
+  expect(GeoMapsClient).toBeDefined();
+  expect(GeoPlacesClient).toBeDefined();
+  expect(GeoRoutesClient).toBeDefined();
+});
+
+test("can import standalone SDKs in sub-modules", () => {
+  expect(Object.keys(maps).length).toBeGreaterThan(0);
+  expect(Object.keys(places).length).toBeGreaterThan(0);
+  expect(Object.keys(routes).length).toBeGreaterThan(0);
+});
